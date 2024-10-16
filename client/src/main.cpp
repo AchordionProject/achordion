9bb57d5dd2b5a759e4b76359b484c2efd7237ac1
Berta Sawa <bertasawa@oakland.edu>
2024-10-16 13:16:50 -0400

    rename WxWidgets.cpp to main.cpp

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,74 +1,99 @@
 #include <wx/wx.h>
 
-class MyApp : public wxApp
-{
+
+using namespace std;
+
+class AchordionApp : public wxApp {
 public:
-    bool OnInit() override;
+	virtual bool OnInit();
 };
-
-wxIMPLEMENT_APP(MyApp);
-
-class MyFrame : public wxFrame
-{
+//THIS WILL BE THE FIRST WINDOW THAT THE USER WILL SEE
+class WelcomeWindow : public wxFrame {
 public:
-    MyFrame();
-
+	WelcomeWindow(const wxString& title);
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+	void OnRecordButtonClick(wxCommandEvent& event);
 };
 
-enum
-{
-    ID_Hello = 1
+class RecordingWindow : public wxFrame {
+public:
+	RecordingWindow(const wxString& title);
+private:
+	void OnRecordingClick(wxCommandEvent& event);
 };
 
-bool MyApp::OnInit()
-{
-    MyFrame *frame = new MyFrame();
-    frame->Show(true);
-    return true;
+bool AchordionApp::OnInit() {
+	WelcomeWindow* frame = new WelcomeWindow("Achordion App");
+	frame->Show(true);
+	return true;
+
 }
 
-MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
+WelcomeWindow::WelcomeWindow(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(700, 700)) {
+	//creating menu bar
+	wxMenu* FileMenu = new wxMenu;
+	FileMenu->Append(wxID_EXIT, "&EXIT");
+	wxMenu* HelpMenu = new wxMenu;
+	HelpMenu->Append(wxID_ABOUT, "&ABOUT");
+	//creating menus to the menu bar
+	wxMenuBar* menuBar = new wxMenuBar;
+	menuBar->Append(FileMenu, "&File");
+	menuBar->Append(HelpMenu, "&Help");
+	SetMenuBar(menuBar);
 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
+	//making a button that will display on the welcome screen
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+	//can adjust sizes here
+	wxButton* record = new wxButton(panel, wxID_ANY, "CLICK TO RECORD", wxPoint(300, 300), wxSize(120, 70));
 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
+	//you have made a record button....  now you must bind it 
+	record->Bind(wxEVT_BUTTON, &WelcomeWindow::OnRecordButtonClick, this);
 
-    SetMenuBar( menuBar );
-
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+	//must also bind all menu buttons
+	Bind(wxEVT_MENU, &WelcomeWindow::OnExit, this, wxID_EXIT);
+	Bind(wxEVT_MENU, &WelcomeWindow::OnAbout, this, wxID_ABOUT);
 }
 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
+//it is bind... but what do you want to do with it? here is where you tell the app what to do
+
+void WelcomeWindow::OnExit(wxCommandEvent& event) {
+	Close(true);
 }
 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+void WelcomeWindow::OnAbout(wxCommandEvent& event) {
+	//WHAT EXACTLY SHOULD THE ABOUT SAY????
+	wxMessageBox("Achordion app for detecting chords??");
 }
 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
+void WelcomeWindow::OnRecordButtonClick(wxCommandEvent& event) {
+	Close(true);
+	RecordingWindow* recording = new RecordingWindow("Ready to record!");
+	recording->Show(true);
 }
+
+//when clicked... we enter the recording frame... here is where it is implemented
+
+RecordingWindow::RecordingWindow(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(700, 700)) {
+	//make a button
+
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+	wxButton* startRecording = new wxButton(panel, wxID_ANY, "CLICK TO START", wxPoint(300, 300), wxSize(120, 100));
+	//binding the button that was mentioned in the RecordingWindow
+	startRecording->Bind(wxEVT_BUTTON, &RecordingWindow::OnRecordingClick, this);
+}
+//***************************************************************************
+//         THIS IS WHERE THE RECORDING LOGIC SHOULD GO
+//			INSTEAD OF WXMESSAGEBOX... WE SHOULD IMPLEMENT AUDIO INPUT LOGIC
+//********************************************************
+void RecordingWindow::OnRecordingClick(wxCommandEvent& event) {
+	wxMessageBox("INSERT RECORDING LOGIC");
+}
+
+
+
+
+
+
+wxIMPLEMENT_APP(AchordionApp);  // This macro creates the main() function for the application
