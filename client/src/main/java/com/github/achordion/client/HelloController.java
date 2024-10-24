1f97085d25b49bcbf1c4ee5442e10cfe66c98b10
phil <pseros@oakland.edu>
2024-10-23 20:10:30 -0400

    Started making the first window. Next Step: Change vbox to something that allows "Click for student" and "Click for teacher" to be aligned properly

--- client/src/main/java/com/github/achordion/client/HelloController.java
+++ client/src/main/java/com/github/achordion/client/HelloController.java
@@ -2,13 +2,39 @@ package com.github.achordion.client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    //this "AnyText" can be used over and over again with different text inside
+    //it has Welcome to Achordion.... then helps display the user input to the screen
+    private Label AnyText;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Label outputLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onStartPageButtonClick() {
+        AnyText.setText("Welcome to Achordion!");
     }
+
+    @FXML
+    //so far it only displays what the user entered for debugging pursposes
+    public void onUserInput(){
+        String inputText = textField.getText();
+        AnyText.setText(inputText);
+        textField.clear();
+    }
+    //this will call onUserInput when the enter button is pressed inside the text field
+    @FXML
+    public void keyPressed(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER){
+            onUserInput();
+        }
+    }
+
 }
