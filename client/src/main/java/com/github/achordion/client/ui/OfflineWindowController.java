10da34908ff72104708a9249382b8ad4163a21d2
Philip Seros <PhilSeros@gmail.com>
2024-11-08 20:32:29 -0500

    Began implementing offline page (1 chord to rule them all!!!)

--- client/src/main/java/com/github/achordion/client/ui/OfflineWindowController.java
+++ client/src/main/java/com/github/achordion/client/ui/OfflineWindowController.java
@@ -1,4 +1,42 @@
 package com.github.achordion.client.ui;
 
+import com.github.achordion.client.protocol.core.MType;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.net.URL;
+
 public class OfflineWindowController {
+    //create a choice box
+    @FXML
+    public ChoiceBox<String> Abox;
+
+    private AudioRecorder audioRecorder = new AudioRecorder();
+    public void initialize() {
+        Abox.getItems().addAll("A sharp","A minor");
+
+    }
+
+    public void AchordClicked(MouseEvent event) {
+        String pitchSelected = Abox.getValue();
+        if (pitchSelected != null && pitchSelected.equals("A sharp")) {
+            //if A sharp is selected.. insert file path to play it
+            System.out.println("A sharp was clicked");
+
+           //This is the relative path to the file inside the project
+            String audioFilePath = "/com/github/achordion/client/Chords/c-major.wav";
+
+            //a URL object can be converted to a file
+            //URL object is universal.... relative path can be ambiguous
+            URL audioURL = getClass().getResource(audioFilePath);
+
+            //the URL object path is read
+            File audioFile = new File(audioURL.getPath());
+
+            audioRecorder.playAudio(audioFile);
+        }
+    }
 }
